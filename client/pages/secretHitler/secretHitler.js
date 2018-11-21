46daed7e2390c8f6430b8175b9b8cfa59417ad22
Limepop <45116284+Limepop@users.noreply.github.com>
2018-11-21 15:57:53 -0600

    added back indentions
    de-crowds change reviewer

--- client/pages/secretHitler/secretHitler.js
+++ client/pages/secretHitler/secretHitler.js
@@ -208,11 +208,11 @@ function setup() {
         for(var i = 0; i < data.va.length; i++){
             chat(data.va[i] + " voted AGAINST this government!", 'red');
         }
-
+        
         for(var i = 0; i < data.vf.length; i++){
             chat(data.vf[i] + " voted FOR this government!", 'green');
         }
-
+        
         chat("The election failed. The next nominee for " + nameOfPresident + " is " + data.presNom);
         $('#' + data.chan).attr('class', 'player');
         $('#' + data.pres).attr('class', 'player');
@@ -240,14 +240,14 @@ function setup() {
     socket.on('voting-passed', function (data) {
         president = data.pres;
         chancellor = data.chan;
-
+        
         for(var i = 0; i < data.va.length; i++){
             chat(data.va[i] + " voted AGAINST this government!", 'red');
         }
         for(var i = 0; i < data.vf.length; i++){
             chat(data.vf[i] + " voted FOR this government!", 'green');
         }
-
+        
         chat("The election was successful. " + president + " is the new " + nameOfPresident + " and " + chancellor + " is the new " + nameOfChancellor + ".", 'cyan');
         if (president == currentUser) {
             presChoosePolicies(data.top);
